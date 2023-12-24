3010af06ba48f84e3b1ce37519b671dc1e9203e8
Konstantin <turbo.foxy01@gmail.com>
2023-12-24 21:19:59 +0100

    new functions added and some sprites updated

--- scripts/gameplay.gd
+++ scripts/gameplay.gd
@@ -4,9 +4,9 @@ var text_box = preload("res://scenes/text_box.tscn")
 
 const characters={
 	"bottom":preload("res://assets/sprites/bottom_sprite.png"),
-	"crowd":preload("res://assets/sprites/crowd_sprite.png"),
+	
 	"helena":preload("res://assets/sprites/Helena_sprite.png"),
-	"hermia":preload("res://assets/sprites/Hermia_sprite.png"),
+	"hermia":preload("res://assets/sprites/hermia_sprite.png"),
 	"lysander":preload("res://assets/sprites/Lysander_sprite.png"),
 	"oberon":preload("res://assets/sprites/oberon_sprite.png"),
 	"puck":preload("res://assets/sprites/Puck_sprite.png"),
@@ -20,28 +20,40 @@ const backgounds = {
 	
 	
 }
+const sounds = {
+	"text_appear": preload("res://assets/sounds/rpg-text-speech-sound-131477-[AudioTrimmer.com].mp3"),
+	"calm_song":preload("res://assets/sounds/music/04 Mitsukiyo 04 Lovely Picnic.mp3")
+}
 #POSSIBLE INSTRUCTIONS
 var text_to_add = ""
 var current_text_speed 
 var default_current_text_speed
-func change_text (speaker :String = "NONE" ,newtext: String="NONE", text_speed: float = 50.0):
+var text_delay_punctuation = {
+	"!":0.15,
+	".":0.15,
+	",":0.10,
+	"—":0.5,
+	"?":0.15
+}
+
+func change_text (speaker :String = "NONE" ,newtext: String="NONE", text_speed_ms: float = 35.0):
 	var wrapper =  func():
 		
 		#if newtext!="NONE":
 			#$text_box/Sprite2D/Label_text.text= newtext
 		$text_box/Sprite2D/Label_text.text =""
 		text_to_add = newtext
-		default_current_text_speed = text_speed/1000
-		current_text_speed = text_speed/1000
+		default_current_text_speed = text_speed_ms/1000
+		current_text_speed = text_speed_ms/1000
 		if speaker!="NONE":
 			$text_box/Sprite2D/Label_name.text =speaker
 	return wrapper
 #x value 0 to -1152
 func add_characters (characters_to_add: Array = [], x_coordinates :Array = []):
 	var new_sprite
-	var normal_width_const = 281.0
-	var normal_height_const = 435.0
-	var normal_y_const = 155.0
+	var normal_width_const = 400.0
+	var normal_height_const = 400.0
+	var normal_y_const =165.0
 	#adds zeros for the x_coordinates as the default value when the x_coordinates are not filled correctly
 	for i in range(characters_to_add.size()-x_coordinates.size()):
 		x_coordinates.append(0)
@@ -55,18 +67,21 @@ func add_characters (characters_to_add: Array = [], x_coordinates :Array = []):
 				new_sprite.texture=characters[characters_to_add[idx]]
 				new_sprite.name = characters_to_add[idx]
 				new_sprite.position.x = x_coordinates[idx]
+					
 				
 				
 				new_sprite.scale= Vector2(
-					#normal_width_const / float(new_sprite.texture.get_width()), #changes the width to match all the others
-					1,
+					normal_width_const / float(new_sprite.texture.get_width()), #changes the width to match all the others
+					#1,
 					normal_height_const / float(new_sprite.texture.get_height())
 				)
 
 				new_sprite.position.y = normal_y_const*(1+0.3*2)
 				
 				new_sprite.z_index = -1
-				
+				#mirror a sprite if it is on the other side
+				if new_sprite.position.x < get_viewport().size.x/2:
+					new_sprite.scale.x *=-1
 				add_child(new_sprite)
 				
 	return wrapper
@@ -99,22 +114,106 @@ func change_background(new_background: String):
 		)
 		$background.position.y += $background.texture.get_height()*screen_height/float($background.texture.get_height())/2
 		$background.position.x += $background.texture.get_width()*screen_width/float($background.texture.get_width())/2
-
 	return wrapper
+
+		
+#this function changes the variables and the result gets calculated in _process
+var new_pos
+var cutscene_active = false
+var cutscene_type = "Linear"
+var character_name_to_animate
+var cutscene_duration = 3.0
+var animation_delay = 0
+var character_speed
+func cutscene(name: String, new_position: float,time : float = 2, animation_type: String ="Linear", delay: float=0.0):
+	name = name.to_lower()
+	var wrapper = func ():
+		character_name_to_animate = get_node(name)
+		
+		if !character_name_to_animate:
+			cutscene_active =false
+		else:
+			new_pos = new_position
+			
+			character_speed = (new_position-character_name_to_animate.position.x)/time
+			cutscene_active = true
+			cutscene_type = animation_type
+
+			cutscene_duration = time
+			animation_delay = delay
+	return wrapper
+
+func change_song(song_name:String):
+	var wrapper = func():
+		if song_name in sounds:
+			$songspeaker.stream = sounds[song_name]
+			$songspeaker.play()
+	return wrapper
+var sound_delay = 0.0
+var play_sound_effect = false
+func play_sound(sound_name:String, sound_delay_seconds = 0.0):
+	var wrapper = func():
+		if sound_name in sounds:
+			$speechspeaker/soundspaeker.stream = sounds[sound_name]
+			sound_delay = sound_delay_seconds
+			play_sound_effect = true
+			
+	return wrapper
+func END():
+	var wrapper = func():
+		get_tree().quit()
+	return wrapper
+			
 
 
 
 #instructions used per scene (index indecates the clicks made counting from 1)
-var scene_instructions = {
+'''var scene_instructions = {
 1: [change_text("Demetrius", "Hallo"), add_characters(["helena"], [900]), add_characters(["lysander"],[500]), change_background("castle") ],
-2: [change_text("Helena", "Hallo")],
+2: [change_text("Helena", "Hallo"), cutscene('helena',90)],
 3: [change_text("Demetrius", "BYE")],
 4: [change_text("HELENA" , "BYE")],
 5:[change_text("end")]
 }
+'''
+var scene_instructions = {
+	1: [change_text("Bottom", "Hey, fellow thespians! Our time to shine is here. But, heads up, we gotta tweak some things."), add_characters([ "bottom"],[200]), change_background('theatre'), change_song('calm_song'), cutscene("bottom", 900,2,'Linear',2)],
+	2: [change_text("Quince", "Tweaks, Bottom? Spill the tea, please.")],
+	3: [change_text("Bottom", "For the ladies, dear Quince! Can't mess with their vibes, you know? Delicate sensibilities and all that jazz.")],
+	4: [change_text("Snug", "And to avoid confusion, let's be crystal—our lion ain't real, and our sword is just for show.")],
+	5: [change_text("Starveling", "Show, got it! Wise moves, Snug. But seriously, no real lion? Bummer.")],
+	6: [change_text("Bottom", "Also, let's spice it up. How 'bout someone plays a wall, and another brings the moonlight with a bush and lantern? Cool, huh?")],
+	7: [change_text("Puck", "*Mischievously:* Let's sprinkle a bit of chaos from the shadows!")],
+	8: [change_text("Bottom", "*Aside: Ah, friends, I'm stepping aside for a quick brainstorm on our epic play. Maybe even drop a sonnet about this wild night.*"), add_characters(["puck"],[900])],
+	9: [change_text("Puck", "*Mischievously:* Now, time for a little mayhem!")],
+	10: [change_text("Craftsmen", "*Terrified: An ass-headed monster! Run for your lives!*")],
+	11: [change_text("Puck", "*Chasing after: Oh, the joy of chaos! Just another day in the enchanted forest, right?*")],
+	12: [change_text("Titania", "*Awakening: What's this? An ass-headed delight! How charming!*")],
+	13: [change_text("Bottom", "*Unaware: Fear not, fair Titania! Bottom is at your service. And yes, my head's up for grabs!*")],
+	14: [change_text("Titania", "*Fair fairies, attend to him. Peaseblossom, Cobweb, Mote, Mustardseed, be his devoted companions. Don't skimp on the head rubs!*")],
+	15: [change_text("Puck", "To Oberon: My lord, all's a bit wild. The lovers roam the forest, love-confused and all.")],
+	16: [change_text("Oberon", "Great, Puck! The plan unfolds. Now, let's sort this mess.")],
+	17: [change_text("Puck", "On it, my lord. No more love confusion. I'll untangle the lovers with the dawn. It's like fixing a magical Rubik's Cube.")],
+	18: [change_text("Oberon", "And our queen enchanted by an ass! This night's a riot.")],
+	19: [change_text("Puck", "Chuckles: Indeed, my lord. The night's mischief knows no bounds. It's almost as fun as pranking mortals.")],
+	20: [change_text("Oberon", "Well, Puck, let the mortals sleep. We'll watch the sunrise and revel in the magic of a midsummer night's dream.")],
+	21: [change_text("Puck", "*Overseeing the lovers as they wake up. Time to see how our little love potion plays out!*")],
+	22: [change_text("Oberon", "Impressed: Look, Puck. The lovers are waking up, and all confusion has vanished.")],
+	23: [change_text("Puck", "Grinning: A little mischief can lead to clarity, my lord. Who knew?")],
+	24: [change_text("Oberon", "Nods: True, Puck. Sometimes, a touch of magic is all they need. We should start a magical advice column, don't you think?")],
+	25: [change_text("Puck", "Gazes at the sunrise: A beautiful end to a magical night. And now, we wait for the reviews from our enchanted audience.")],
+	26: [change_text("Oberon", "Agrees: Indeed, Puck. Let's leave them to their day. The magic of the night has done its work. Until the next night, my mischievous companion.")],
+	27: [change_text("Puck", "Vanishing into the shadows: Until then, my lord. Remember, the night is young, and so are we.")],
+	28: [change_text("Oberon", "Fading away: Until then, Puck. The magic will continue.")],
+	29: [END()]
+}
+
+
+
+
 var click_counter = 1
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	#initial call
 
@@ -129,10 +228,47 @@ func _process(delta):
 	#let letters manualy appear
 	current_text_speed-=delta
 	if text_to_add!="" and current_text_speed<=0:
+		current_text_speed = default_current_text_speed
+		if text_to_add[0] in text_delay_punctuation:
+			current_text_speed+=text_delay_punctuation[text_to_add[0]]
 		$text_box/Sprite2D/Label_text.text+=text_to_add[0]
 		text_to_add = text_to_add.substr(1)
-		current_text_speed = default_current_text_speed
+		
 		$speechspeaker.play()
+		
+	
+	#cutscene behaviour
+	
+	if cutscene_active and animation_delay<=0:
+		
+		
+		
+		cutscene_duration-=delta
+		match (cutscene_type):
+			
+			"Linear":
+				character_name_to_animate.position.x+=character_speed*delta
+			"Quadratic":
+				pass
+	#sound behaviour
+	if sound_delay<0 and play_sound_effect:
+		$speechspeaker/soundspaeker.play()
+		play_sound_effect = false
+	elif play_sound_effect:
+		sound_delay-=delta
+		
+		
+		
+				
+	#animation behaviour
+	if animation_delay>0:
+		animation_delay-=delta
+	if cutscene_duration <=0:
+		cutscene_active = false
+		
+		
+		
+		
 		
 		
 	
@@ -147,8 +283,8 @@ func _input(event: InputEvent):
 						
 						
 					
-				if scene_instructions.keys().max()<click_counter+1:
-					get_tree().quit()
+				#if scene_instructions.keys().max()<click_counter+1:
+					#get_tree().quit()
 					
 				
 				for i in scene_instructions[click_counter]:
@@ -163,5 +299,5 @@ func _input(event: InputEvent):
 			
 
 				
-			if event.button_index == MOUSE_BUTTON_RIGHT:
-				pass
+			#if event.button_index == MOUSE_BUTTON_RIGHT:
+				#pass
---
+++ scripts/start_credits.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var time_to_wait = 5
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if time_to_wait<=0:
+		get_tree().change_scene_to_file("res://scenes/main menu.tscn")
+	time_to_wait-=delta
